a651b0842b6a87dc09f3ccfe0186187e18488280
daniel <danicek76@yahoo.com>
2021-02-03 15:39:46 +0100

    added another random color solution in app.js

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const btn = document.querySelector("button")
 const body = document.querySelector("body")
 function backgroundChanger() {
-  let r = Math.floor(Math.random() * 255)
-  let g = Math.floor(Math.random() * 255)
-  let b = Math.floor(Math.random() * 255)
+  let randomColor = function randomNumber(min, max) {
+    return Math.round(Math.random() * (max - min)) + min
+  }
+  let r = randomColor(0, 255)
+  let g = randomColor(0, 255)
+  let b = randomColor(0, 255)
+
   return `rgb(${r}, ${g}, ${b})`
 }
 
 btn.addEventListener("click", () => {
   body.style.backgroundColor = backgroundChanger()
 })
+// another solution
+
+// const btn = document.querySelector("button")
+// const body = document.querySelector("body")
+// function backgroundChanger() {
+//   let r = Math.floor(Math.random() * 255)
+//   let g = Math.floor(Math.random() * 255)
+//   let b = Math.floor(Math.random() * 255)
+//   return `rgb(${r}, ${g}, ${b})`
+// }
+
+// btn.addEventListener("click", () => {
+//   body.style.backgroundColor = backgroundChanger()
+// })
